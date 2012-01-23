1235c0987c0c59674c609475ef8731d607074cb4
Colin Alstad <colin.alstad@gmail.com>
2012-01-23 13:05:34 -0500

    Add file for Mac OS X specific customizations.

---
+++ darwin.el
@@ -0,0 +1,8 @@
+;; Set meta to apple key
+(setq mac-command-modifier 'meta)
+
+;; Keep visual bell from displaying black box in cocoa emacs
+(setq ring-bell-function 'ignore)
+
+;; Set font
+(set-face-font 'default "-apple-inconsolata-medium-r-normal--14-0-72-72-m-0-iso10646-1")
