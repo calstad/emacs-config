f2f14e66211881404de4ef1d5815a3c0e49d6d25
Colin Alstad <colin.alstad@gmail.com>
2012-01-23 21:59:49 -0500

    Add customization file for magit.

---
+++ customizations/my-magit.el
@@ -0,0 +1,15 @@
+;; Customizations for the magit emacs interface to git.
+
+;; Make sure the package is installed
+(ensure-package 'magit)
+
+;; Set keybinding for magit status
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; Diff colors
+(eval-after-load 'magit
+  '(progn
+     (set-face-foreground 'magit-diff-add "green3")
+     (set-face-foreground 'magit-diff-del "red3")))
+
+(provide 'my-magit)
