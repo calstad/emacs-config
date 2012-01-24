fb8cb7f5da4051d3f8afe963350fc9d26d2e589b
Colin Alstad <colin.alstad@gmail.com>
2012-01-23 20:56:39 -0500

    Fix typo in using quotes instead of ticks.

--- utils/backup-autosave-mgr.el
+++ utils/backup-autosave-mgr.el
@@ -7,11 +7,12 @@
     (make-directory user-temporary-file-directory t)
     (setq backup-by-copying t)
     (setq backup-directory-alist
-          '((".*" . ,user-temporary-file-directory)
+          `((".*" . ,user-temporary-file-directory)
             (,tramp-file-name-regexp nil)))
     (setq auto-save-list-file-prefix
           (concat user-temporary-file-directory ".auto-saves-"))
     (setq auto-save-file-name-transforms
-          '((".*" ,user-temporary-file-directory t)))))
+          `((".*" ,user-temporary-file-directory t)))))
 
 (provide 'backup-autosave-mgr)
+
