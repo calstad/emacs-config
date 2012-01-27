f88ae0c6fd237d4dbd6fc0d3892c73d589fa724c
Colin Alstad <colin.alstad@gmail.com>
2012-01-27 11:37:02 -0500

    Custom file created by customization buffer.  Not sure if a good idea to keep under source control or not.

---
+++ custom.el
@@ -0,0 +1,12 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("ac4e4935b5103cefd6c634667a5d7a1a0bb4dc5756ade85c831c189632034383" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
