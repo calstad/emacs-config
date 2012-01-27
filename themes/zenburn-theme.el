330a6849258fd7303d89f5a16b4da6e6aef2909a
Colin Alstad <colin.alstad@gmail.com>
2012-01-27 11:30:58 -0500

    Update zenburn theme to use darker background when showing mis/matching parens.

--- themes/zenburn-theme.el
+++ themes/zenburn-theme.el
@@ -419,8 +419,8 @@
    `(rpm-spec-var-face ((,class (:foreground ,zenburn-red))))
 
    ;; show-paren
-   `(show-paren-mismatch ((,class (:foreground ,zenburn-red-3 :background ,zenburn-bg :weight bold))))
-   `(show-paren-match ((,class (:foreground ,zenburn-blue-1 :background ,zenburn-bg :weight bold))))
+   `(show-paren-mismatch ((,class (:foreground ,zenburn-red-3 :background ,zenburn-bg-1 :weight bold))))
+   `(show-paren-match ((,class (:foreground ,zenburn-blue-1 :background ,zenburn-bg-1 :weight bold))))
 
    ;; SLIME
    `(slime-repl-inputed-output-face ((,class (:foreground ,zenburn-red))))
