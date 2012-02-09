33609b2ee382f5de4d67538782a9f695ba9c2f9b
Colin Alstad <colin.alstad@gmail.com>
2012-02-09 11:52:41 -0500

    Add hooks and configs for use in all programming modes.

--- customizations/programming-config.el
+++ customizations/programming-config.el
@@ -1,5 +1,14 @@
+;; Hooks for all programming modes
+(add-hook 'prog-mode-hook 'cf-local-column-number-mode)
+(add-hook 'prog-mode-hook 'cf-local-comment-auto-fill)
+(add-hook 'prog-mode-hook 'cf-turn-on-hl-line-mode)
+(add-hook 'prog-mode-hook 'cf-pretty-lambdas)
+(add-hook 'prog-mode-hook 'cf-add-watchwords)
 
-;; Configs for programming modes
+(defun custom-prog-mode-hook ()
+  (run-hooks 'prog-mode-hook))
+
 (require 'magit-config)
+(require 'lisp-config)
 
 (provide 'programming-config)
