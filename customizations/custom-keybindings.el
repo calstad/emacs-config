1ecc3ec002ec3cab3022b92935bb3a15824cf20d
Colin Alstad <colin.alstad@gmail.com>
2012-02-09 13:26:59 -0500

    Add a lot of custom key bindings taken from esk and prelude.

--- customizations/custom-keybindings.el
+++ customizations/custom-keybindings.el
@@ -1,5 +1,8 @@
+;; Align your code in a pretty way.
+(global-set-key (kbd "C-x \\") 'align-regexp)
+
 ;; It's all about the project.
-(global-set-key (kbd "C-c f") 'find-file-in-project)
+(global-set-key (kbd "C-x f") 'find-file-in-project)
 
 ;; You know, like Readline.
 (global-set-key (kbd "C-M-h") 'backward-kill-word)
@@ -9,5 +12,29 @@
 
 ;; Perform general cleanup.
 (global-set-key (kbd "C-c n") 'cf-cleanup-buffer)
+
+;; Jump to a definition in the current file. (Protip: this is awesome.)
+(global-set-key (kbd "C-x C-i") 'imenu)
+
+;; Use regex searches by default.
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "\C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "M-%") 'query-replace-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+(global-set-key (kbd "C-M-%") 'query-replace)
+
+
+;; Start eshell or switch to it if it's active.
+(global-set-key (kbd "C-x m") 'eshell)
+
+;; Start a new eshell even if one is active.
+(global-set-key (kbd "C-x M") (lambda () (interactive) (eshell t)))
+
+;; If you want to be able to M-x without meta
+(global-set-key (kbd "C-c x") 'execute-extended-command)
+
+;; swap windows
+(global-set-key (kbd "C-c s") 'cf-swap-windows)
 
 (provide 'custom-keybindings)
