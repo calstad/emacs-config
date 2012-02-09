632a21a331dd97d1e69a42d813966e39b51558b0
Colin Alstad <colin.alstad@gmail.com>
2012-02-09 11:54:02 -0500

    Add file for customizations of all lisp modes.

---
+++ customizations/lisp-config.el
@@ -0,0 +1,36 @@
+(ensure-package 'paredit)
+
+;; Emacs Lisp
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'emacs-lisp-mode-hook 'cf-remove-elc-on-save)
+(add-hook 'emacs-lisp-mode-hook 'custom-prog-mode-hook)
+(add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode)
+
+(defun cf-remove-elc-on-save ()
+  "If you're saving an elisp file, likely the .elc is no longer valid."
+  (make-local-variable 'after-save-hook)
+  (add-hook 'after-save-hook
+            (lambda ()
+              (if (file-exists-p (concat buffer-file-name "c"))
+                  (delete-file (concat buffer-file-name "c"))))))
+
+;; Customizations for interactive elisp mode
+(defun cf-ielm-mode-hook ()
+  (cf-turn-on-paredit)
+  (turn-on-eldoc-mode))
+
+(add-hook 'ielm-mode-hook 'cf-ielm-mode-hook)
+
+(define-key emacs-lisp-mode-map (kbd "C-c v") 'eval-buffer)
+
+;; General Lisp configs
+
+(define-key read-expression-map (kbd "TAB") 'lisp-complete-symbol)
+(define-key lisp-mode-shared-map (kbd "RET") 'reindent-then-newline-and-indent)
+
+(dolist (mode '(scheme emacs-lisp lisp clojure clojurescript))
+  (when (> (display-color-cells) 8)
+    (add-hook (intern (concat (symbol-name mode) "-mode-hook"))
+              'cf-turn-on-paredit)))
+
+(provide 'lisp-config)
--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 ;; Where all the magic starts!
 
+(require 'cl)
+
 ;; Turn off mouse interface early in startup to avoid momentary display
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
