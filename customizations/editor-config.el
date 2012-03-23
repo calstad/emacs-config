00856b643628172a11fdfde95a36f070efd3c119
Colin Alstad <colin.alstad@gmail.com>
2012-03-23 10:29:47 -0400

    Rearrange hippie expand lookups.

--- customizations/editor-config.el
+++ customizations/editor-config.el
@@ -32,12 +32,12 @@
                                          try-expand-dabbrev-all-buffers
                                          try-expand-dabbrev-from-kill
                                          try-complete-file-name-partially
-                                         try-complete-file-name
                                          try-expand-all-abbrevs
                                          try-expand-list
                                          try-expand-line
                                          try-complete-lisp-symbol-partially
-                                         try-complete-lisp-symbol))
+                                         try-complete-lisp-symbol
+                                         try-complete-file-name))
 
 ;; Smart indenting and pairing for all
 (electric-pair-mode t)
