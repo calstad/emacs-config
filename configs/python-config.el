24a9a01d9a80059838f83c6f2a694b7855999cbb
Colin Alstad <colin.alstad@gmail.com>
2013-04-17 11:09:24 -0400

    Set ipython as default inferior python process.

--- configs/python-config.el
+++ configs/python-config.el
@@ -1,3 +1,19 @@
+;; Set IPython as inferior-python process.
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args ""
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
+;; Use default indent level of 4 spaces
+(setq python-indent-guess-indent-offset nil)
+
 (defun calstad-python-mode-defaults ()
   (calstad-turn-off-electric-indent))
 
