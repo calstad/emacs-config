737da461b2c4a853646543a39926082835e8dd7a
Colin Alstad <colin.alstad@gmail.com>
2015-01-10 16:16:44 -0500

    Remove marmalade as a package repo.

--- configs/elpa-package-config.el
+++ configs/elpa-package-config.el
@@ -4,8 +4,6 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (package-initialize)
 
