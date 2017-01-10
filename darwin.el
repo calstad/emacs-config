21ffd74b400727e2dc67f9da244550adcd6f4e38
Colin Alstad <colin.alstad@gmail.com>
2017-01-10 13:11:58 -0500

    Remove all old configuration.

--- darwin.el
+++
@@ -1,9 +0,0 @@
-;; Set meta to apple key
-(setq mac-command-modifier 'meta)
-
-;; Keep visual bell from displaying black box in cocoa emacs
-(setq ring-bell-function 'ignore)
-
-;; Set font
-(if window-system
-    (setq default-frame-alist '((font . "-apple-inconsolata-medium-r-normal--16-0-72-72-m-0-iso10646-1"))))
