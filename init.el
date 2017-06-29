527a062a3b8ba7bfdd5990745992e7c023a73e4f
Colin Alstad <colin.alstad@gmail.com>
2017-06-29 18:07:02 -0400

    Move to use-package macro for package configuration management

--- init.el
+++ init.el
@@ -1,10 +1,3 @@
-(require 'package)
-(add-to-list 'package-archives '
-	     ("org" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-
 (require 'org-install)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "emacs-config.org" user-emacs-directory))
