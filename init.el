82ab128836bce9cc5ba435db07f349a039f57300
Colin Alstad <colin.alstad@gmail.com>
2020-10-10 22:42:07 -0400

    Fix issue in org-mode from straight not loading

--- init.el
+++ init.el
@@ -1,4 +1,16 @@
-(package-initialize)
-(require 'org-install)
-(require 'ob-tangle)
+;; Install straight.el to manage packages
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+;; Have straight immediately load org-mode so the correct version is loaded
+(straight-use-package 'org)
 (org-babel-load-file (expand-file-name "emacs-config.org" user-emacs-directory))
